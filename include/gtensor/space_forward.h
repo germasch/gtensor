b16b92420fd7a470e2f93d9f43ace9a1917f7070
Kai Germaschewski <kai.germaschewski@unh.edu>
2021-05-02 23:32:37 -0400

    wip

--- include/gtensor/space_forward.h
+++ include/gtensor/space_forward.h
@@ -9,22 +9,38 @@ namespace gt
 namespace space
 {
 
-struct host
+struct host_only
 {};
+
 #ifdef GTENSOR_USE_THRUST
 struct thrust
 {};
 #endif
+
 #ifdef GTENSOR_DEVICE_CUDA
 struct cuda
 {};
+struct cuda_managed
+{};
+struct cuda_host
+{};
 #endif
+
 #ifdef GTENSOR_DEVICE_HIP
 struct hip
 {};
+struct sycl_managed
+{};
+struct sycl_host
+{};
 #endif
+
 #ifdef GTENSOR_DEVICE_SYCL
 struct sycl
+{};
+struct sycl_managed
+{};
+struct sycl_host
 {};
 #endif
 
@@ -39,10 +55,12 @@ using device = hip;
 #elif GTENSOR_DEVICE_SYCL
 using device = sycl;
 #endif
+using host = host_only;
 
 #else // !  GTENSOR_HAVE_DEVICE
 
 using device = host;
+using host = host_only;
 
 #endif
 
